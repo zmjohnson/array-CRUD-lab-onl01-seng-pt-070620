0ff2fc0787a2a997b456792cdcf00b19b29afdf7
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-07 20:43:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["xbox", "pc", "playstation", "switch"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << (create_an_array,"gameboy")
 end
 
 def add_element_to_start_of_array(array, element)
