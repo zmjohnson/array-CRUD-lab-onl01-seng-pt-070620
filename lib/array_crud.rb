a102d9eede7be91e3c6d3c2cc65d0904374e95fa
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-07 20:44:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << (create_an_array,"gameboy")
+  add_element_to_end_of_array << ("gameboy")
 end
 
 def add_element_to_start_of_array(array, element)
